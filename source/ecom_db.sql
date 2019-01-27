c6f80c3b2b8ac519368e9a15467fbc33c4ea5bb8
jayvee <mendozajayveeb@gmail.com>
2019-01-27 15:47:42 +0800

    db sql included

--- source/ecom_db.sql
+++ source/ecom_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2019 at 04:41 AM
+-- Generation Time: Jan 27, 2019 at 08:45 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -38,10 +38,9 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Pants'),
-(2, 'Polo'),
-(3, 'Shoes'),
-(4, 'Coat');
+(1, 'Classic Favorites'),
+(2, 'Specialty Breads'),
+(3, 'Doughnuts');
 
 -- --------------------------------------------------------
 
@@ -63,8 +62,24 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`id`, `name`, `price`, `description`, `img_path`, `category_id`) VALUES
-(19, 'Test1', '20.00', 'asda', '../assets/images/jkr2.jpg', 1),
-(20, 'Test2', '20.00', 'qwert', '../assets/images/jkr2.jpg', 2);
+(21, 'Chocolate Chip Bread Loaf', '128.00', 'Machine-sliced chocolate chip bread loaf.', '../assets/images/cbreadloaf.jpg', 1),
+(22, 'Classic Buchi', '65.00', 'Mini buchi bread topped with sesame seeds and onion chives.', '../assets/images/cbuchi.jpg', 1),
+(23, 'Fudgee Ensaymada', '139.50', 'Our very own ensaymada topped with peanut butter and sprinkled with ground coffee beans from Thailand.', '../assets/images/censaymada.jpg', 1),
+(24, 'Spanish Bread', '69.00', 'Hand-made buttery spanish bread made to perfection.', '../assets/images/cspanish.jpg', 1),
+(25, 'Apple Cider Donut', '83.00', 'Sweet apple cider donut with cinnamon.', '../assets/images/dappleciderdn.jpg', 3),
+(26, 'Donutella', '87.00', 'Nutella-stuffed classic doughnut.', '../assets/images/ddonutella.jpg', 3),
+(27, 'Doughnuthan', '69.00', 'Doughnut made for Jonathan. ', '../assets/images/ddonuthandn.jpg', 3),
+(28, 'Creamy Pie', '69.00', 'Special doughnut for your love ones.', '../assets/images/dcreampie.jpg', 3),
+(29, 'Ice Cream Doughnuts', '120.00', 'Ice cream inspired doughnuts for your sweet tooth.', '../assets/images/dicecreamdn.jpeg', 3),
+(30, 'Princess Doughnut', '320.00', 'Doughnut made for royalties.', '../assets/images/dprincessdn.jpg', 3),
+(31, 'Red Velvet', '230.00', 'Blockbuster doughnut made with fresh strawberries and dark chocolate that you should not miss.', '../assets/images/dredvelvet.jpg', 3),
+(32, 'Vegan Doughnuts', '65.00', 'Worry no more with our low-calorie doughnut for people who still wants to lose weight and enjoy life at the same time.', '../assets/images/dvegandn.jpg', 3),
+(33, 'Brasserie', '380.00', 'Unsweetened recipe bread from France at the lowest price.', '../assets/images/sbrasserie.jpg', 2),
+(34, 'Chocolate Wheat Bread', '286.00', 'Special chocolate wheat bread for stronger immunity and caloric surplus.', '../assets/images/schoco.jpg', 2),
+(35, 'Olive Bread', '194.75', 'Gluten-free olive bread that is good for the heart.', '../assets/images/solive.jpg', 2),
+(36, 'Breadzza', '340.00', 'Bread pizza or pizza bread? You decide.', '../assets/images/spizza.jpg', 2),
+(37, 'Pumpkin Bread', '99.95', 'Special pumpkin bread with lutein for good eyesight.', '../assets/images/spumpkin.jpg', 2),
+(38, 'Swiss County Bread', '800.00', 'Special swiss county bread from county of swiss topped with nuts, almonds, and hazelnuts. *made to order*', '../assets/images/sswisscountry.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -89,7 +104,12 @@ INSERT INTO `orders` (`id`, `user_id`, `transaction_code`, `purchase_date`, `sta
 (1, 6, '5DDEFDAB21A1A937-1548469014', '2019-01-25 19:16:54', 3, 1),
 (2, 6, 'B1CE65BDB573911C-1548470834', '2019-01-25 19:47:14', 3, 1),
 (3, 6, '9ECEE77FABD7686C-1548472415', '2019-01-25 20:13:35', 2, 1),
-(4, 6, '683EE89D7EC87337-1548473782', '2019-01-25 20:36:22', 1, 1);
+(4, 6, '683EE89D7EC87337-1548473782', '2019-01-25 20:36:22', 3, 1),
+(5, 6, '28B47F0CE0824D00-1548474235', '2019-01-25 20:43:55', 3, 1),
+(6, 6, '6B79756395F11893-1548493193', '2019-01-26 01:59:53', 3, 1),
+(7, 6, '871F240253D48905-1548563475', '2019-01-26 21:31:15', 3, 1),
+(8, 6, 'E148AD6140DF1260-1548571497', '2019-01-26 23:44:57', 2, 1),
+(9, 6, '680EDBA4FDC2374A-1548573100', '2019-01-27 00:11:40', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -104,17 +124,6 @@ CREATE TABLE `order_items` (
   `price` decimal(18,2) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `order_items`
---
-
-INSERT INTO `order_items` (`id`, `order_id`, `item_id`, `price`, `quantity`) VALUES
-(1, 1, 19, '20.00', 2),
-(2, 1, 20, '20.00', 5),
-(3, 2, 19, '20.00', 1),
-(4, 3, 19, '20.00', 2),
-(5, 4, 20, '20.00', 1);
 
 -- --------------------------------------------------------
 
@@ -266,25 +275,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `payment_modes`
